4f570d7fb9814e994291f7a0b3ffc76f322fe98c
sunilshw <sunil1.todkar@gmail.com>
2019-10-15 13:55:04 +0530

    2

--- js/main.js
+++ js/main.js
@@ -19,7 +19,13 @@ $(document).ready(function(){
   //------- Active Nice Select --------//
      $('select').niceSelect();
     
-
+     var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+     var element = document.getElementById('text');
+     if (isMobile) {
+      $("nav").toggleClass('hide');
+      $("span", this).toggleClass("lnr-menu lnr-cross");
+      $(".main-menu").addClass('mobile-menu');
+     }
 
     $('.active-slider').owlCarousel({
         center: true,
@@ -113,37 +119,7 @@ $(document).ready(function(){
 
     
 
-      // -------   Mail Send ajax
 
-         $(document).ready(function() {
-            var form = $('#myForm'); // contact form
-            var submit = $('.submit-btn'); // submit button
-            var alert = $('.alert-msg'); // alert div for show alert message
-
-            // form submit event
-            form.on('submit', function(e) {
-                e.preventDefault(); // prevent default form submit
-
-                $.ajax({
-                    url: 'mail.php', // form action url
-                    type: 'POST', // form submit method get/post
-                    dataType: 'html', // request type html/json/xml
-                    data: form.serialize(), // serialize form data
-                    beforeSend: function() {
-                        alert.fadeOut();
-                        submit.html('Sending....'); // change submit button text
-                    },
-                    success: function(data) {
-                        alert.html(data).fadeIn(); // fade in response data
-                        form.trigger('reset'); // reset form
-                        submit.attr("style", "display: none !important");; // reset submit button text
-                    },
-                    error: function(e) {
-                        console.log(e)
-                    }
-                });
-            });
-        });
-
+       
 
  });
